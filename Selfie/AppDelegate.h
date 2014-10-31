f0b32822ba7beffe67e3218d95b0577b91a9e85d
luk4shubhs <luk4shubhs@gmail.com>
2014-10-31 18:27:09 +0530

    Initial Commit

---
+++ Selfie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Selfie
+//
+//  Created by Shubham Sorte on 31/10/14.
+//  Copyright (c) 2014 Apps2eaze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Selfie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Selfie
+//
+//  Created by Shubham Sorte on 31/10/14.
+//  Copyright (c) 2014 Apps2eaze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
